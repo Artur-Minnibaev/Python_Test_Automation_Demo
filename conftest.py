460bb41ca39d5c2bccc31183c25da7cd4e39ff95
artur.minnibaev <artur.minnibaev11@gmail.com>
2024-03-01 14:14:44 -0800

    first actions

--- conftest.py
+++ conftest.py
@@ -12,9 +12,22 @@ def browser():
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     # options.add_argument("--start-maximized")
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f"Screenshot {datetime.today}", attachment_type=allure.attachment_type.PNG)
     driver.quit()
+
+# @pytest.fixture(scope="session", autouse=True)
+# def browser():
+#     # Note: Please specify your chromedriver local path here:
+#     options = webdriver.ChromeOptions()
+#     driver = webdriver.Remote('http://localhost:4444/wd/hub', options=options)
+#     yield driver
+#     attach = driver.get_screenshot_as_png()
+#     allure.attach(attach, name=f"Screenshot {datetime.today}", attachment_type=allure.attachment_type.PNG)
+#     driver.quit()
+
+
+
