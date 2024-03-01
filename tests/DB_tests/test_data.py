69e39b345142b11c608a4a0ff4e57b74f3f1aabf
artur.minnibaev <artur.minnibaev11@gmail.com>
2024-03-01 14:02:11 -0800

    first actions

--- tests/DB_tests/test_data.py
+++ tests/DB_tests/test_data.py
@@ -1,11 +1,6 @@
 import pytest
-import psycopg2
-from fixtures import connection_db
 
 
-def test_select_data(setup_database):
-    cur = setup_database.cursor()
-    param = cur.execute("SELECT * FROM items_app WHERE title='Git Pocket Guide'")
-    print(param)
-    # assert param ==
+def test_select_data():
+    pass
 
